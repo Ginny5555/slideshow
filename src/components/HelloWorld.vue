c93555864d40db4cde4a59cfea5ae37cf10f685a
Ginny5555 <spass_kaya@mail.ru>
2023-04-20 02:17:35 +0300

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,138 +1,202 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="">
+    <div class="hello container">
+      <span @click="prevSlide" class="lightgrey prev">&#9668;</span>
+      <div class="slider-container">
+        <ul class="slider" :style="styleObject" v-if="posts && posts.length">
+          <li
+            class="green slide"
+            v-for="post of posts"
+            :key="post"
+            @click="() => onSelect(post)"
+          >
+            <div
+              :class="{ active: showMobileMenu }"
+              @click="showMobileMenu = !showMobileMenu"
+            >
+              <img :src="post.download_url" alt />
+            </div>
+          </li>
+        </ul>
+      </div>
+      <span @click="nextSlide" class="lightgrey next">&#9658;</span>
+    </div>
     <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
+      <h2>Click on the image</h2>
+      <li v-for="(item, index) in clickedItems" :key="index" class="fade-in">
+        <p>{{ item.url }}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      posts: "",
+      slides: 100,
+      active: 0,
+      activeSlide: 1,
+      clickedItems: [],
+      showMobileMenu: false,
+    };
+  },
+  created() {
+    axios
+      .get("https://picsum.photos/v2/list?page=2&limit=15")
+      .then((response) => {
+        this.posts = response.data.map((post) => ({
+          ...post,
+        }));
+        console.log(this.posts);
+      });
+  },
+  computed: {
+    styleObject: function () {
+      const width = 2 - (this.activeSlide - 0) * 230;
+      return {
+        transform: "translateX(" + width + "px)",
+      };
+    },
+  },
+  methods: {
+    changeSlide(num) {
+      this.activeSlide = num + 1;
+    },
+    nextSlide() {
+      if (this.activeSlide < 10) this.activeSlide++;
+    },
+    prevSlide() {
+      if (this.activeSlide > 0) this.activeSlide--;
+    },
+    onSelect(post) {
+      this.clickedItems.push(post);
+      JSON.stringify(this.clickedItems);
+      console.log(this.clickedItems.toString());
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+html,
+body {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  text-align: center;
+}
+.hello {
+  height: 100%;
+  display: inline-flex;
+  align-items: center;
+}
+.slider-container {
+  overflow: hidden;
+  width: 93%;
+  height: 247px;
+  margin: 0 auto;
+}
+.slider {
+  width: 5000px;
+  height: 400px;
+  overflow: hidden;
+  padding: 0;
+  transition: all 0.32s ease;
+}
+.slide {
+  float: left;
+  width: 200px;
+  height: 200px;
+  font-size: 20px;
+  color: black;
+  padding: 10px;
+  margin: 4px;
+  border: 1px solid #cdae71;
+  border-radius: 11px;
+  box-shadow: 7px 4px 10px 1px rgb(0 0 0 / 15%);
+  &:hover {
+    background-color: rgb(179 179 178);
+  }
+}
+.navigator {
+  text-align: center;
+  display: inline-block;
+  margin: 15px auto;
+  overflow: hidden;
+  padding-bottom: 16px;
+  height: 30px;
+}
+.navigator li {
+  width: 20px;
+  height: 20px;
+  float: left;
+  color: #ecf0f1;
+  border-radius: 50%;
+  padding: 7px;
+  margin: 0 10px;
+  cursor: pointer;
+  box-shadow: 0 3px 17px rgba(0, 0, 0, 0.3);
+  transition: all 0.32s ease;
+  position: relative;
+  background-color: #cdae71;
+  &:hover {
+    transform: translateY(-2px);
+  }
+}
+img {
+  width: 200px;
+  height: 200px;
+}
+.lightgrey {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  float: left;
+  color: #ecf0f1;
+  border-radius: 50%;
+  padding: 7px;
+  margin: 0 10px;
+  cursor: pointer;
+  box-shadow: 0 3px 17px rgba(0, 0, 0, 0.3);
+  transition: all 0.32s ease;
+  position: relative;
+  background-color: #cdae71;
+  &:hover {
+    transform: translateY(-2px);
+  }
+}
+.next {
+  top: 50%;
+}
+.prev {
+  top: 50%;
+}
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+.fade-in {
+  opacity: 0; /* make things invisible upon start */
+  animation: fadeIn ease-in 1;
+  animation-fill-mode: forwards;
+  animation-duration: 1.4s;
+}
+p {
+  font-size: 16px;
 }
 ul {
-  list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="home-header container">
+      <img alt="Vue logo" src="../assets/logo.webp" />
+      <h1>Carousele</h1>
+    </div>
+    <HelloWorld />
   </div>
 </template>
 
@@ -16,3 +19,39 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+img {
+  width: 200px;
+}
+</style>
+<style lang="scss">
+.home {
+  &-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    & h1 {
+      text-transform: uppercase;
+      font-size: 40px;
+    }
+  }
+}
+.container {
+  width: 80%;
+  margin: 0 auto;
+}
+ul {
+  list-style-type: none;
+}
+@media (max-width: 650px) {
+  .container {
+    width: 330px;
+  }
+  .home-header {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
